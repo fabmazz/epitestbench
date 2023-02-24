c7a1fb82d6e95be4da1bffbe61dfe9d3350c58fb
Fabio Mazza <fab4mazz@gmail.com>
2023-02-24 15:30:40 +0100

    Fix generation of obs with fixed I

--- testbench/lib/obs_extra.py
+++ testbench/lib/obs_extra.py
@@ -18,24 +18,33 @@ def gen_obs_single_t(epitrace:np.ndarray, p_choose:float, seed=None, t_obs=None)
 
     return list(zip(idcs, obs, times))
 
-def gen_obs_fixed_inf(times, frac_inf, n_obs_day, T, rng):
+def gen_obs_fixed_inf(times, frac_inf, n_obs_day, T, rng,debug=False):
+    MAX_TRIALS=40
     ti=times[0][0]+1
     tr = ti+times[1]
-    
-    N = len(ti)
+
     isinf=np.where(~np.isinf(ti))[0]
     tot_ninf = len(isinf)
+    extract_inf = True
+    trials=0
+    while extract_inf:
+        nobs_inf = int(rng.rand()+(frac_inf*tot_ninf))
+        rng.shuffle(isinf)
+        ### nodes to be observed
+        nodes_obs=isinf[:nobs_inf]
 
-    nobs_inf = int(rng.rand()+(frac_inf*tot_ninf))
-    rng.shuffle(isinf)
-    ### nodes to be observed
-    nodes_obs=isinf[:nobs_inf]
-
-    ti_obs=ti[nodes_obs]
-    delay_t=(rng.rand(len(ti_obs))*(T-ti_obs+1))
-
-    tobs_inf=(ti_obs+delay_t).astype(int)
-
+        ti_obs=ti[nodes_obs]
+        delay_t=(rng.rand(len(ti_obs))*(T-ti_obs+1))
+        tobs_inf=(ti_obs+delay_t).astype(int)
+        trials +=1
+        if np.all(
+            np.unique(tobs_inf,return_counts=True)[1] <= n_obs_day):
+            extract_inf=False
+        elif trials >= MAX_TRIALS:
+            extract_inf = False
+            ### abandon
+    if debug:
+        print(f"Extract inf took {trials} trials")
     obs_node={t: [] for t in range(0,T+1)}
     ## add i to observe:
     all_obs =[]
@@ -47,9 +56,11 @@ def gen_obs_fixed_inf(times, frac_inf, n_obs_day, T, rng):
         notinf = np.where(ti>t)[0]
         rng.shuffle(notinf)
         #rng.shuffle(notinf)
-        nodes_sel = notinf[:(n_obs_day-len(obs_node[t]))]
-        obs_node[t].extend(nodes_sel)
-        all_obs.extend((i,0,t) for i in nodes_sel)
+        nch=(n_obs_day-len(obs_node[t]))
+        if nch > 0:
+            nodes_sel = notinf[:nch]
+            obs_node[t].extend(nodes_sel)
+            all_obs.extend((i,0,t) for i in nodes_sel)
     
 
     return all_obs
